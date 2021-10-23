a033a8968743fc8d3c17d9d36a7b07c51f9a0c84
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-24 00:36:36 +0900

    feat: Add Header & JejuMap

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Dimensions, ScrollView } from "react-native";
 import styled from "styled-components/native";
 
+const Door = styled.Image``;
+
+const Title = styled.Text`
+  flex: 1;
+  font-size: 27px;
+  font-weight: 800;
+  align-self: center;
+  padding-top: 8px;
+  padding-bottom: 60px;
+`;
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
 export default function App() {
-  return <View></View>;
+  return (
+    <ScrollView style={{ backgroundColor: "#FEF8EA", marginTop: 40 }}>
+      <Door source={require("./images/Header.png")} />
+      <Title>제주 쉬기 딱 좋은 지역별 안내</Title>
+      <Door source={require("./images/JejuMap.png")} />
+    </ScrollView>
+  );
 }
