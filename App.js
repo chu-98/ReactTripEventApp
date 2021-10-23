b2c6157389e623bd176e87862f4af8cd4bab3ece
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-24 01:00:57 +0900

    feat: Complete First Page

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
 import React from "react";
-import { View, Dimensions, ScrollView } from "react-native";
+import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 
 const Door = styled.Image``;
+const First = styled.View``;
 
 const Title = styled.Text`
-  flex: 1;
   font-size: 27px;
   font-weight: 800;
   align-self: center;
   padding-top: 8px;
-  padding-bottom: 60px;
+  padding-bottom: 40px;
 `;
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
-    <ScrollView style={{ backgroundColor: "#FEF8EA", marginTop: 40 }}>
-      <Door source={require("./images/Header.png")} />
-      <Title>제주 쉬기 딱 좋은 지역별 안내</Title>
-      <Door source={require("./images/JejuMap.png")} />
+    <ScrollView
+      pagingEnabled={true}
+      horizontal
+      style={{ marginTop: 45, backgroundColor: "#FEF2C6" }}
+    >
+      <First style={styles.container}>
+        <Door source={require("./images/Header.png")} />
+        <Title>제주 쉬기 딱 좋은 지역별 안내</Title>
+        <Door source={require("./images/JejuMap.png")} />
+      </First>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: SCREEN_WIDTH,
+  },
+});
