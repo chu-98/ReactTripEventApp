0f7c8e5fa81a5653a1ade0266105b71144457c17
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-24 01:36:22 +0900

    feat: Connect Mock Server API with Axios

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import styled from "styled-components/native";
+import axios from "axios";
 
 const Door = styled.Image``;
 const First = styled.View``;
+const Second = styled.View``;
+const Third = styled.View``;
 
 const Title = styled.Text`
   font-size: 27px;
@@ -16,8 +19,21 @@ const Title = styled.Text`
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [locations, setLocations] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        "https://69cee40f-f372-4734-a2ff-a043da89d0b2.mock.pstmn.io/location"
+      )
+      .then(function (response) {
+        console.log(response);
+        setLocations(response);
+      });
+  }, []);
+
   return (
     <ScrollView
+      showsHorizontalScrollIndicator={false}
       pagingEnabled={true}
       horizontal
       style={{ marginTop: 45, backgroundColor: "#FEF2C6" }}
@@ -27,6 +43,8 @@ export default function App() {
         <Title>제주 쉬기 딱 좋은 지역별 안내</Title>
         <Door source={require("./images/JejuMap.png")} />
       </First>
+      <Second style={styles.container}></Second>
+      <Third style={styles.container}></Third>
     </ScrollView>
   );
 }
